6e22d874b578ec9200b6d973d0e31e364793b5f7
AClimber <alex_zasimenko@mail.ru>
2019-05-27 21:20:16 +0300

    add SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { AppComponent } from "./app/pages/App";
+import "./app/styles/main.scss";
 
 render(<AppComponent/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, "src/index.tsx"),
@@ -9,7 +10,7 @@ module.exports = {
         publicPath: '/'
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".css"]
     },
     devServer: {
         historyApiFallback: true
@@ -20,12 +21,21 @@ module.exports = {
                 test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: extractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader']
+                    })
             }
         ]
     },
     plugins: [
         new htmlWebpackPlugin({
             template: path.join(__dirname, "/src/index.html")
-        })
+        }),
+        new extractTextPlugin('bundle.css')
     ]
 };
